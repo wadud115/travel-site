ef4fbf47cb885f8740abeec17379ead16d1daf01
wadud115 <wadud871226@gmail.com>
2026-09-11 20:39:45 +0600

    first commit

---
+++ src/components/Nabver.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Nabver = () => {
+    return (
+        <nav className='flex justify-between p-5 shadow-xs'>
+            <ul className='flex gap-3'>
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/destination'}>Destination</Link></li>
+                <li><Link href={'/my-bookings'}>My Bookings</Link></li>
+            </ul>
+
+
+            <div>
+                <Image src={'/assets/Wanderlast.png'}
+                width={150}
+                height={150}
+                alt='logo'></Image>
+            </div>
+
+
+             <ul className='flex gap-3' >
+                <li><Link href={'/profile'}>Profile</Link></li>
+                <li><Link href={'/login'}>Login</Link></li>
+                <li><Link href={'/signup'}>Sign Up</Link></li>
+            </ul>
+            
+        </nav>
+    );
+};
+
+export default Nabver;
