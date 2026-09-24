27872b1e73a16c273512d7f8d8c56c2abff383d2
wadud115 <wadud871226@gmail.com>
2026-09-24 16:36:06 +0600

    email authentication add

---
+++ src/app/login/page.jsx
@@ -0,0 +1,102 @@
+'use client'
+
+import { authClient } from '@/lib/auth-client';
+import { Button, Card, Description, FieldError, Form, Input, Label, TextField } from '@heroui/react';
+import { redirect } from 'next/navigation';
+import React from 'react';
+
+const LogInPage = () => {
+
+    const onSubmit = async(e)=>{
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const user = Object.fromEntries(formData.entries());
+        console.log(user)
+
+        const {data , error } = await authClient.signIn.email({
+            
+            password : user.password,
+           
+            email : user.email
+        })
+
+
+        console.log({data,error})
+
+        if(data){
+            redirect('/')
+        }
+    }
+    return (
+        <div className='max-w-7xl mx-auto my-10'>
+
+            <div className='my-3 text-center'>
+                <h1 className='font-bold text-2xl'>Log in</h1>
+                <p className='text-gray-600'>Start your adventure with Wanderlust</p>
+            </div>
+
+
+            <Card className='p-5 rounded-none'>   
+                
+                    <Form onSubmit={onSubmit} className="flex w-96 flex-col gap-4">
+
+
+
+      <TextField
+        isRequired
+        name="email"
+        type="email"
+        validate={(value) => {
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            return "Please enter a valid email address";
+          }
+          return null;
+        }}
+      >
+        <Label>Email</Label>
+        <Input placeholder="john@example.com" />
+        <FieldError />
+      </TextField>
+
+
+
+      
+  
+      <TextField
+        isRequired
+        minLength={8}
+        name="password"
+        type="password"
+        validate={(value) => {
+          if (value.length < 8) {
+            return "Password must be at least 8 characters";
+          }
+          if (!/[A-Z]/.test(value)) {
+            return "Password must contain at least one uppercase letter";
+          }
+          if (!/[0-9]/.test(value)) {
+            return "Password must contain at least one number";
+          }
+          return null;
+        }}
+      >
+        <Label>Password</Label>
+        <Input placeholder="Enter your password" />
+        <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+        <FieldError />
+      </TextField>
+      <div className="flex gap-2">
+        <Button className={'bg-cyan-500 rounded-none w-full'} type="submit">
+          
+          Login
+        </Button>
+       
+      </div>
+    </Form></Card>
+     
+        </div>
+    );
+};
+
+export default LogInPage;
--- src/components/Nabver.jsx
+++ src/components/Nabver.jsx
@@ -1,10 +1,26 @@
+"use client"
+
+import { authClient } from '@/lib/auth-client';
+import { Avatar, Button } from '@heroui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const Nabver = () => {
+
+    const { 
+        data: session, 
+    } = authClient.useSession() 
+
+    // console.log(session)
+
+    const user = session?.user;
+
+    console.log(user)
+
+
     return (
-        <nav className='flex justify-between p-5 shadow-xs'>
+        <nav className='flex items-center justify-between p-5 shadow-xs'>
             <ul className='flex gap-3'>
                 <li><Link href={'/'}>Home</Link></li>
                 <li><Link href={'/destinations'}>Destination</Link></li>
@@ -21,10 +37,30 @@ const Nabver = () => {
             </div>
 
 
-             <ul className='flex gap-3' >
-                <li><Link href={'/profile'}>Profile</Link></li>
+             <ul className='flex items-center gap-3' >
+                   <li><Link href={'/profile'}>Profile</Link></li>
+                { user? <>
+
+
+                <li>
+                     <Avatar>
+        <Avatar.Image alt="John Doe" src={user?.image} />
+        <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+      </Avatar>
+                </li>
+                <li><Button variant='danger' className={'rounded-none'}>Log out</Button></li>
+                
+                </> 
+                
+                :
+                
+                <>
                 <li><Link href={'/login'}>Login</Link></li>
                 <li><Link href={'/signup'}>Sign Up</Link></li>
+                
+                </>}
+                
+                
             </ul>
             
         </nav>
